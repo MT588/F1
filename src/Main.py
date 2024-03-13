7f4fa50a82416f04b1c226425b3569795b2d7930
maarten-vandun <maarten.vandun@student.kuleuven.be>
2024-03-13 14:00:23 +0100

    data inserted

---
+++ src/Main.py
@@ -0,0 +1,23 @@
+DriverData = {
+    "VER": {"price": 30.4, "Race1": 45, "Race2": 36},
+    "PER": {"price": 22.1, "Race1": 31, "Race2": 31},
+    "LEC": {"price": 20.4, "Race1": 22, "Race2": 37},
+    "SAI": {"price": 18.8, "Race1": 36, "Race2": 0},
+    "RUS": {"price": 19.2, "Race1": 20, "Race2": 15},
+    "HAM": {"price": 19.5, "Race1": 12, "Race2": 6},
+    "NOR": {"price": 23.2, "Race1": 16, "Race2": 8},
+    "PIA": {"price": 19.4, "Race1": 10, "Race2": 23},
+    "ALO": {"price": 16.2, "Race1": 7, "Race2": 16},
+    "STR": {"price": 11.0, "Race1": 8, "Race2": -17},
+    "ALB": {"price": 7.3, "Race1": 0, "Race2": 6},
+    "SAR": {"price": 5.9, "Race1": 3, "Race2":7},
+    "RIC": {"price": 8.7, "Race1": 5, "Race2": 0},
+    "TSU": {"price": 7.6, "Race1": -1, "Race2": -1},
+    "BOT": {"price": 6.0, "Race1": 0, "Race2": -1},
+    "ZHO": {"price": 6.9, "Race1": 11, "Race2": -2},
+    "HUL": {"price": 6.7, "Race1": -3, "Race2": 9},
+    "MAG": {"price": 7.2, "Race1": 7, "Race2": 7},
+    "OCO": {"price": 8.8, "Race1": 7, "Race2": 8},
+    "GAS": {"price": 7.5, "Race1": 6, "Race2": -20}
+}
+
